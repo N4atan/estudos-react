cf0fe8f408f6908879975ad9c21ea020e5f7a105
Natan <162522588+N4atan@users.noreply.github.com>
2025-06-10 11:26:46 -0300

    Add files via upload

---
+++ flashcards-game/src/App.jsx
@@ -0,0 +1,5 @@
+import DivMain from './components/DivMain'
+
+export default function App() {
+  return <DivMain />
+}
---
+++ flashcards-game/src/components/DivMain.jsx
@@ -0,0 +1,7 @@
+export default function DivMain(){
+    return (
+        <div id="div-main" className="w-3/4 h-3/4 bg-neutral-100 mx-auto mt-15">
+
+        </div>
+    )
+}

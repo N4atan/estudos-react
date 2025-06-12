47fdfb54671ee17a29af20e6791d7bd65b177998
Natan <162522588+N4atan@users.noreply.github.com>
2025-06-12 11:32:26 -0300

    Add files via upload

--- flashcards-game/src/components/DivMain.jsx
+++ flashcards-game/src/components/DivMain.jsx
@@ -1,7 +1,17 @@
 export default function DivMain(){
     return (
-        <div id="div-main" className="w-3/4 h-3/4 bg-neutral-100 mx-auto mt-15">
+        <main id="div-main" className="w-3/4 h-3/4 bg-neutral-100 mx-auto mt-15 p-4 gap-5 flex flex-col items-center">
+            <header className="w-9/10 h-14 bg-neutral-200">
 
-        </div>
+            </header>
+
+            <div className="w-4/5 h-3/4 bg-neutral-300 flex justify-center items-center">
+                <h1>Fault</h1>
+            </div>
+
+            <footer className="w-9/10 h-18 bg-neutral-400">
+
+            </footer>
+        </main>
     )
 }
